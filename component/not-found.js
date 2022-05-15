dc77483f97293fadd530ed2f333bb76b443ce031
Avinash Sharma <58262578+avi9360@users.noreply.github.com>
2022-05-15 13:21:43 +0530

    UI improvement

--- component/not-found.js
+++ component/not-found.js
@@ -3,7 +3,7 @@ import { Heading, Box } from "@chakra-ui/react";
 const NotFound = () => {
   return (
     <Box height="100vh" display="flex" justifyContent="center" alignItems="center" flexDirection="column">
-      <Heading>Invalid URL</Heading>
+      <Heading >Invalid URL</Heading>
     </Box>
   );
 };
